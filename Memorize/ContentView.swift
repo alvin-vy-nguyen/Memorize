3cff39ae5dcdacd7a3053ae98a5319f1d3c12d13
Alvin Nguyen <alvin.nguyen@veeva.com>
2024-04-23 19:57:06 -0700

    Togglable isFaceUp

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -4,7 +4,6 @@
 //
 //  Created by Alvin Nguyen on 4/20/24.
 //
-//
 
 import SwiftUI
 
@@ -22,19 +21,22 @@ struct ContentView: View {
 }
 
 struct CardView: View {
-    var isFaceUp: Bool = false
+    @State var isFaceUp = false
     
     var body: some View {
         ZStack {
+            let base = RoundedRectangle(cornerRadius: 12)
             if isFaceUp {
-                RoundedRectangle(cornerRadius: 12)
-                    .foregroundColor(.white)
-                RoundedRectangle(cornerRadius: 12)
-                    .strokeBorder(lineWidth: 2)
+                base.fill(.white)
+                base.strokeBorder(lineWidth: 2)
                 Text("🐳").font(.largeTitle)
             } else {
-                RoundedRectangle(cornerRadius: 12)
+                base
             }
+        }
+        .onTapGesture {
+            //isFaceUp = !isFaceUp is the same as:
+            isFaceUp.toggle()
         }
     }
 }
